19901f8928f3afcf2a02e4a5aaedf7a2c7cbaa8a
callcolor <call.color@gmail.com>
2025-07-05 16:18:47 -0500

    Update config.example.ts

--- config.example.ts
+++ config.example.ts
@@ -3,5 +3,6 @@ export const firstName = "BonnieBelle86";
 export const lastName = "Resident";
 export const password = "REPLACE_ME_WITH_BOT_ACCOUNT_CREDENTIALS";
 
-// Secret key.
+// Create any custom secret key.  
+// This will be used by your in-world LSL scripts to authenticate.
 export const secret = "REPLACE_ME_WITH_A_SECRET_KEY";
